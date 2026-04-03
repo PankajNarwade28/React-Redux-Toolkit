c6c453258178a3f141585756251acca125c69921
PankajNarwade28 <pankajnarwade258@gmail.com>
2026-04-03 15:51:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+ 
+import './App.css'
+import { CounterUI } from './app/components/CounterUI'
+
+function App() { 
+
+  return (
+    <> 
+        {/* Hello World  */}
+        <CounterUI  />
+    </>
+  )
+}
+
+export default App
---
+++ src/app/components/CounterUI.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from "../../features/counterSlice.jsx";
+
+export const CounterUI = () => {
+  const count = useSelector((state) => state.counter.value);
+  const [amount, setAmount] = useState(0);
+  const dispatch = useDispatch();
+  return (
+    <div className="container">
+      <div className="counter-card">
+        <h1>Current Count</h1>
+        <div className="count-display">{count}</div>
+
+        <div className="button-group">
+          <button onClick={() => dispatch(decrement())}>- Decrease</button>
+          <button onClick={() => dispatch(increment())}>+ Increase</button>
+          <input
+            type="text"
+            name="count"
+            id=""
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <button
+            className="btn-add-5"
+            onClick={() => dispatch(incrementByAmount(Number(amount) || 0))}
+          >
+            + Add Amount
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
